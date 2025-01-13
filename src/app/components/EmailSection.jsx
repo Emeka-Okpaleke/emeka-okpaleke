3a5682a0c9289a7dd64e9b74209c65b8ebc9260e
Emeka-Okpaleke <olisemekaokpaleke@yahoo.com>
2025-01-13 07:06:16 +0100

    LOGO added

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/Emeka-Okpaleke">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/olisemeka-okpaleke-9087a82b3/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
