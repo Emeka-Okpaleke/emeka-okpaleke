2a91dfb5f9ba2d8e4d389961018a2aa5bab4e51e
Emeka-Okpaleke <olisemekaokpaleke@yahoo.com>
2025-02-07 08:39:31 +0100

    email description updated

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -53,7 +53,7 @@ const EmailSection = () => {
           {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hello, I&apos;ll
-          try my best to get back to you!
+          try my best to get back to you
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/Emeka-Okpaleke">
